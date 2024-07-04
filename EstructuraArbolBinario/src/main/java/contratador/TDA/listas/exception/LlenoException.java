ea4e63b80fec9fbf263e3962d34a1d70bb38280e
Jossibel Perez <jossibel.perez@unl.edu.ec>
2024-07-03 23:09:04 -0500

    Arreglo2

--- EstructuraArbolBinario/src/main/java/contratador/TDA/listas/exception/LlenoException.java
+++ EstructuraArbolBinario/src/main/java/contratador/TDA/listas/exception/LlenoException.java
@@ -1,13 +1,29 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
  */
-package contratador.TDA.listas.exception;
+package controlador.TDA.listas.exception;
 
 /**
  *
- * @author Anahi Perez
+ * @author sebastian
  */
-public class LlenoException {
-    
+public class LlenoException extends Exception {
+
+    /**
+     * Creates a new instance of <code>VacioException</code> without detail
+     * message.
+     */
+    public LlenoException() {
+    }
+
+    /**
+     * Constructs an instance of <code>VacioException</code> with the specified
+     * detail message.
+     *
+     * @param msg the detail message.
+     */
+    public LlenoException(String msg) {
+        super(msg);
+    }
 }
