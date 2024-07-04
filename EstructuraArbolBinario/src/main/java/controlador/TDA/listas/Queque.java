97d96692476a9b7a3ce50c0263c026a3f17a9d3b
Jossibel Perez <jossibel.perez@unl.edu.ec>
2024-07-03 23:17:41 -0500

    Arreglo 3

---
+++ EstructuraArbolBinario/src/main/java/controlador/TDA/listas/Queque.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador.TDA.listas;
+
+import controlador.TDA.listas.exception.LlenoException;
+import controlador.TDA.listas.exception.VacioException;
+
+/**
+ *
+ * @author sebastian
+ */
+public class Queque<E> {
+    private QuequeOperation<E> quequeOperation;
+    public Queque(Integer cant) {
+        this.quequeOperation = new QuequeOperation<>(cant);
+    }
+    public void queque(E dato) throws LlenoException {
+        quequeOperation.queque(dato);
+    }
+    
+    public Integer getSize() {
+        return this.quequeOperation.getSize();
+    }
+    
+    public void clear() {
+        this.quequeOperation.clear();
+    }
+    
+    public Integer getTop() {
+        return this.quequeOperation.getTop();
+    }
+    
+    public void print() {
+        System.out.println("Cola");
+        System.out.println(quequeOperation.print());
+        System.out.println("******");
+    }
+    //TODO.. falta push
+    public E dequeque() throws VacioException {
+        return quequeOperation.dequeque();
+    }
+}
+
+
+
+
+
