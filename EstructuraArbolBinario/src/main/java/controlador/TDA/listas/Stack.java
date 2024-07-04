0e53d3655b56921c12ecd9b311be4e2e12e99743
mechm <maria.chuico@unl.edu.ec>
2024-07-03 23:19:19 -0500

    Creacion de Clases
    Clases QuequeOperation, Stack y StackOperation

---
+++ EstructuraArbolBinario/src/main/java/controlador/TDA/listas/Stack.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador.TDA.listas;
+
+/**
+ *
+ * @author Maria Chuico
+ */
+public class Stack {
+    
+}
